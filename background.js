fc1c350ce182f2cc22590a82696d70650882d8a6
ekilah <monroe.ekilah@gmail.com>
2013-02-26 13:05:04 -0800

    Now works with arbitrary URLs! Fixed #1.
    Any url with a number at the end, prefixed by a forward slash, and an optional slash after and/or a 5-char extension (including the decimal point) should work. xkcd works :)

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
+ var pattern = /(.*\/)([\d]+)([\/]?[.\w]{0,5}$)/; //if the URL ends in a number, which is preceeded by a forward slash, and optionally followed by a slash and/or optionally followed by an extension of up to 5 characters (including decimal)
+ 
  function checkForUrlMatch(tabId, changeInfo, tab) {
+  var isGoodUrl =  tab.url.match(pattern);
+  
+  //tab.url.match(/github\.com/);
  // If the URL of the tab contains the string github.com
-  var isGoodUrl =  tab.url.match(/github\.com/);
   if (isGoodUrl) {
    //show page action on this tab
    chrome.pageAction.show(tabId);
@@ -24,26 +28,30 @@ chrome.extension.onMessage.addListener(
 		if(request.msg == "increment"){
 			console.log("INCREMENT from background");
 			chrome.tabs.getSelected(null, function(tab){
-				var pattern = /issues\/[\d]+/;
-				var newnum  = parseInt(pattern.exec(tab.url)[0].substring(7));//will silently fail if URL does not match pattern and nothing will be changed about URL
-				console.log("Int parsed for incrementation was: " + newnum);
-				newnum++;
-				var newurl = tab.url.replace(/issues\/[\d]+/, "issues/"+newnum);
-				
+				var oldurl = tab.url;
+				var newurl = oldurl.replace(pattern, function(fullMatch, pre, num, post){
+					console.log("Pre: " + pre);
+					console.log("Int parsed for incrementation was: " + num);
+					console.log("Post: " + post);
+					return pre + (Number(num)+1) + post;
+				});
+				console.log("New URL: " + newurl);
 				chrome.tabs.update(tab.tabId, {url: newurl});
 			
 			});
 			}else if(request.msg=="decrement"){
 				console.log("DECREMENT from background");
 				chrome.tabs.getSelected(null, function(tab){
-					var pattern = /issues\/[\d]+/;
-					var newnum  = parseInt(pattern.exec(tab.url)[0].substring(7));//will silently fail if URL does not match pattern and nothing will be changed about URL
-					console.log("Int parsed for decrementation was: " + newnum);
-					newnum--;
-					var newurl = tab.url.replace(/issues\/[\d]+/, "issues/"+newnum);
-					
+					var oldurl = tab.url;
+					var newurl = oldurl.replace(pattern, function(fullMatch, pre, num, post){
+						console.log("Pre: " + pre);
+						console.log("Int parsed for decrementation was: " + num);
+						console.log("Post: " + post);
+						return pre + (Number(num)-1) + post;
+					});
+					console.log("New URL: " + newurl);
 					chrome.tabs.update(tab.tabId, {url: newurl});
-					
+				
 				});
 			}
 	}
--- popup.js
+++ popup.js
@@ -1,34 +1,40 @@
 
+var pattern = /(.*\/)([\d]+)([\/]?[.\w]{0,5}$)/; //if the URL ends in a number, which is preceeded by a forward slash, and optionally followed by a slash and/or optionally followed by an extension of up to 5 characters (including decimal)
+
 function increment(){
 	console.log("INCREMENT");
 	chrome.tabs.getSelected(null, function(tab){
-		var pattern = /issues\/[\d]+/;
-		var newnum  = parseInt(pattern.exec(tab.url)[0].substring(7));
-		console.log("Int parsed for incrementation was: " + newnum);
-		newnum++;
-		var newurl = tab.url.replace(/issues\/[\d]+/, "issues/"+newnum);
-		
-		chrome.tabs.update(tab.tabId, {url: newurl});
-		
-	});
+				var oldurl = tab.url;
+				var newurl = oldurl.replace(pattern, function(fullMatch, pre, num, post){
+					console.log("Pre: " + pre);
+					console.log("Int parsed for incrementation was: " + num);
+					console.log("Post: " + post);
+					return pre + (Number(num)+1) + post;
+				});
+				console.log("New URL: " + newurl);
+				chrome.tabs.update(tab.tabId, {url: newurl});
+			
+			});
 }
 
 function decrement(){
 	console.log("DECREMENT");
 	
 	chrome.tabs.getSelected(null, function(tab){
-		var pattern = /issues\/[\d]+/;
-		var newnum  = parseInt(pattern.exec(tab.url)[0].substring(7));
-		console.log("Int parsed for decrementation was: " + newnum);
-		newnum--;
-		var newurl = tab.url.replace(/issues\/[\d]+/, "issues/"+newnum);
-		
+		var oldurl = tab.url;
+		var newurl = oldurl.replace(pattern, function(fullMatch, pre, num, post){
+			console.log("Pre: " + pre);
+			console.log("Int parsed for decrementation was: " + num);
+			console.log("Post: " + post);
+			return pre + (Number(num)-1) + post;
+		});
+		console.log("New URL: " + newurl);
 		chrome.tabs.update(tab.tabId, {url: newurl});
-		
+				
 	});
 }
 
-// Run our kitten generation script as soon as the document's DOM is ready.
+// Add action listeners for buttons as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
   console.log("LOADED!!");
   document.getElementById("btnIncrement").addEventListener('click',increment);
