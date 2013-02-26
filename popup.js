2af4f40424591dabb649c493d6cb7c6583a12772
ekilah <monroe.ekilah@gmail.com>
2013-02-26 13:32:29 -0800

    Made the regex pattern in popup be fetched from the background page's version rather than storing it in both places. This was done to centralize this pattern data so it can potentially be changed on the fly and be updated in popups on the fly.

--- popup.js
+++ popup.js
@@ -1,26 +1,28 @@
 
-var pattern = /(.*\/)([\d]+)([\/]?[.\w]{0,5}$)/; //if the URL ends in a number, which is preceeded by a forward slash, and optionally followed by a slash and/or optionally followed by an extension of up to 5 characters (including decimal)
+//var pattern = /(.*\/)([\d]+)([\/]?[.\w]{0,5}$)/; //if the URL ends in a number, which is preceeded by a forward slash, and optionally followed by a slash and/or optionally followed by an extension of up to 5 characters (including decimal)
 
 function increment(){
 	console.log("INCREMENT");
 	chrome.tabs.getSelected(null, function(tab){
-				var oldurl = tab.url;
-				var newurl = oldurl.replace(pattern, function(fullMatch, pre, num, post){
-					console.log("Pre: " + pre);
-					console.log("Int parsed for incrementation was: " + num);
-					console.log("Post: " + post);
-					return pre + (Number(num)+1) + post;
-				});
-				console.log("New URL: " + newurl);
-				chrome.tabs.update(tab.tabId, {url: newurl});
-			
-			});
+		var pattern = chrome.extension.getBackgroundPage().pattern;
+		console.log("Pattern retrieved from background: " + pattern);
+		var oldurl = tab.url;
+		var newurl = oldurl.replace(pattern, function(fullMatch, pre, num, post){
+			console.log("Pre: " + pre);
+			console.log("Int parsed for incrementation was: " + num);
+			console.log("Post: " + post);
+			return pre + (Number(num)+1) + post;
+		});
+		console.log("New URL: " + newurl);
+		chrome.tabs.update(tab.tabId, {url: newurl});
+	
+	});
 }
 
 function decrement(){
 	console.log("DECREMENT");
-	
 	chrome.tabs.getSelected(null, function(tab){
+		var pattern = chrome.extension.getBackgroundPage().pattern;
 		var oldurl = tab.url;
 		var newurl = oldurl.replace(pattern, function(fullMatch, pre, num, post){
 			console.log("Pre: " + pre);
